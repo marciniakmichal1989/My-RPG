9bfba366daf1890f2c81ff4696973066cec102bc
marciniakmichal1989 <marciniakmichal1989@gmail.com>
2020-11-10 11:49:40 +0100

    Day2
    Basic Movement - finished, Basic Combat part 1

---
+++ My-RPG/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = _target.position;
+
+
+    }
+}
---
+++ My-RPG/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    
+    //[SerializeField] private Transform _target;
+    
+
+    void Update()
+    {
+
+
+        UpdateAnimator();
+    
+    }
+
+
+
+    public void MoveTo(Vector3 destynation)
+    {
+        GetComponent<NavMeshAgent>().destination = destynation;
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed); 
+    }
+
+}
